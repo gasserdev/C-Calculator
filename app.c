06d7a836a0ea0d595ce195254e4f0fbff804e4a3
Gasser Zayed <gasserzayed.work0@gmail.com>
2025-12-26 19:30:14 +0200

    Implement simple calculator in C

---
+++ app.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Welcome to Simple C Calculator!\n");
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator); 
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            break;
+
+        case '/':
+            if(num2 != 0)
+                result = num1 / num2;
+            else {
+                printf("Error: Division by zero!\n");
+                return 1; 
+            }
+            break;
+
+        default:
+            printf("Error: Invalid operator!\n");
+            return 1;
+    }
+
+    printf("Result: %.2lf\n", result); 
+
+    return 0;
+}
